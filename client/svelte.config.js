3bbc222d1396252a39408b4515cfb57685e4d937
Lucien Fiorini <lucien.fiorini@student.uclouvain.be>
2024-06-24 16:03:39 +0200

    update deps

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -3,17 +3,10 @@ import adapter from "@sveltejs/adapter-node";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for more information about preprocessors
-  preprocess: [
-    preprocess({
-      postcss: true,
-    }),
-  ],
-
-  kit: {
-    // hydrate the <div id="svelte"> element in src/app.html
-    target: "#svelte",
+	kit: {
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter({
       out: "build",
       precompress: false,
@@ -22,7 +15,8 @@ const config = {
         port: "PORT",
       },
     }),
-  },
+	}
 };
+
 
 export default config;
